46d8878bd19399dbce6bba9079e02a7ef8116746
Daniel Teixeira <dant25@gmail.com>
2019-08-13 14:39:51 -0300

    Updates

---
+++ precache-manifest.896c04b04ff810efeeb20b20aacb2cb9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "905f54116a81af1ea581ae322d28c795",
+    "url": "/tindev_frontend/index.html"
+  },
+  {
+    "revision": "7d3d0c0b12d3a364153b",
+    "url": "/tindev_frontend/static/css/main.4660cd6d.chunk.css"
+  },
+  {
+    "revision": "294de1519d8b6a6ae2f3",
+    "url": "/tindev_frontend/static/js/2.dd2f9804.chunk.js"
+  },
+  {
+    "revision": "7d3d0c0b12d3a364153b",
+    "url": "/tindev_frontend/static/js/main.68ebb0bd.chunk.js"
+  },
+  {
+    "revision": "0e4a262d530b57f3674f",
+    "url": "/tindev_frontend/static/js/runtime~main.6c8bd192.js"
+  },
+  {
+    "revision": "d4ec34e81346484c83328c449a35141b",
+    "url": "/tindev_frontend/static/media/dislike.d4ec34e8.svg"
+  },
+  {
+    "revision": "542059a425f4220819edb0e56c735bb7",
+    "url": "/tindev_frontend/static/media/like.542059a4.svg"
+  },
+  {
+    "revision": "6a01ceef657ab22cc670a0a1ae108176",
+    "url": "/tindev_frontend/static/media/logo.6a01ceef.svg"
+  }
+]);
